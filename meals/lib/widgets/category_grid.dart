33220038229a9ecff1088bcbe74248945c303869
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2024-06-08 11:31:27 +0200

    adding meals data and loading that meals data into a screen

--- meals/lib/widgets/category_grid.dart
+++ meals/lib/widgets/category_grid.dart
@@ -9,22 +9,30 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          category.color.withOpacity(0.55),
-          category.color.withOpacity(0.9),
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-      )),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onSurface,
-            ),
+    // GestureDetector does the same thing, but it doesnt give visual feedback when the user clicks on the item
+    // with InkWell you get that visual feedback
+    return InkWell( // wraping the container with this InkWell widget to make it tappable
+      onTap: () {} , // set up a function that is triggered every time is this item is tapped
+      splashColor: Theme.of(context).primaryColor, // base for this effect
+      borderRadius: BorderRadius.circular(16), // allows to define a radius to give items a rounded courners,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.55),
+            category.color.withOpacity(0.9),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        )),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+        ),
       ),
     );
   }
